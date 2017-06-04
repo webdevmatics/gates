85d69ca4426ffade50a8a8ecee5e07e88cbbfe6e
webdevmatics <webdevmatics@gmail.com>
2017-06-04 13:08:17 +0545

    ksd

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -9,7 +9,7 @@ class BotController extends Controller
     public function bot(Request $request)
     {        
     	$data=$request->all();
-    	$id = $data["entry"][0]["messaging"][0]["sender"]["id"];
-    	dd($id);
+    	// $id = $data["entry"][0]["messaging"][0]["sender"]["id"];
+    	dd($data);
     }
 }
