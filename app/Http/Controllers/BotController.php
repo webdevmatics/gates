278f54952ec450beb2691f7f609483284247aef9
webdevmatics <webdevmatics@gmail.com>
2017-06-04 12:46:03 +0545

    bot link added

---
+++ app/Http/Controllers/BotController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BotController extends Controller
+{
+    public function bot(Request $request)
+    {
+    	return $request->all();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/bot','BotController@bot');
+Route::post('/bot','BotController@bot');
