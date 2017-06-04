5e5d64a9d796d94852def077bfb31c1e01163fdb
webdevmatics <webdevmatics@gmail.com>
2017-06-04 13:20:14 +0545

    bot function added

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -8,8 +8,30 @@ class BotController extends Controller
 {
     public function bot(Request $request)
     {        
-    	$data=$request->all();
-    	// $id = $data["entry"][0]["messaging"][0]["sender"]["id"];
-    	dd($data);
+    	 $data = $request->all();
+        //get the user’s id
+        $id = $data["entry"][0]["messaging"][0]["sender"]["id"];
+     $this->sendTextMessage($id, "Hello");
     }
+
+
+    private function sendTextMessage($recipientId, $messageText)
+    {
+        $messageData = [
+            "recipient" => [
+                "id" => $recipientId
+            ],
+            "message" => [
+                "text" => $messageText
+            ]
+        ];
+        $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token=' . env("PAGE_ACCESS_TOKEN"));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($messageData));
+        curl_exec($ch);
+    
+}
 }
