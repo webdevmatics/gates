83a3496c19723779357ec1dd6e38893081ef06b4
webdevmatics <webdevmatics@gmail.com>
2017-06-04 13:07:03 +0545

    bot

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -7,7 +7,9 @@ use Illuminate\Http\Request;
 class BotController extends Controller
 {
     public function bot(Request $request)
-    {
-        return $request->all();
+    {        
+    	$data=$request->all();
+    	$id = $data["entry"][0]["messaging"][0]["sender"]["id"];
+    	dd($id);
     }
 }
