cb778df2e876434f6f1dc5b815765b0b7e1a6dd3
webdevmatics <webdevmatics@gmail.com>
2017-06-04 14:12:02 +0545

    ksjdl

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -9,9 +9,10 @@ class BotController extends Controller
     public function bot(Request $request)
     {   
     	 $data = $request->all();
+    	 dd($data);
         //get the user’s id
-        $id = $data["entry"][0]["messaging"][0]["sender"]["id"];
-     $this->sendTextMessage($id, "Hello");
+     //    $id = $data["entry"][0]["messaging"][0]["sender"]["id"];
+     // $this->sendTextMessage($id, "Hello");
     }
 
 
